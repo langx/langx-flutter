d39afd4d30fe8780c5b3e73793b60f4f73c558c1
David <davidhvse@gmail.com>
2024-06-09 14:51:00 +0200

    Update dfault_buttons.dart

--- lib/dfault_framework/dfault_material/dfault_buttons.dart
+++ lib/dfault_framework/dfault_material/dfault_buttons.dart
@@ -1,40 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/dfault_framework/dfault_material/dfault_colors.dart';
 
 //default ButtonFilled parameters
-const Color activeColor = primaryColor;
 const Color inactiveColor = Color.fromARGB(221, 114, 113, 113);
-
-//default ButtonOutlined parameters
-const Color outlinedColor = primaryColor;
-
-
-//default inkwell parameters
 
 
 Widget dButtonFilled({required onPressed, String text = "", bool active = true}){
 
-  final Color color;
-
-  if (active == true) {
-    color = activeColor;
+  if (active == false) {
+    return ElevatedButton(
+      onPressed: onPressed, 
+      child: Text(text), 
+      style: ElevatedButton.styleFrom(backgroundColor: inactiveColor),
+      );
   } else {
-    color = inactiveColor;
+    return ElevatedButton(
+      onPressed: onPressed, 
+      child: Text(text), 
+      );
   }
-
-  return ElevatedButton(
-    onPressed: onPressed, 
-    child: Text(text), 
-    style: ElevatedButton.styleFrom(backgroundColor: color),
-    );
-
-}
-
-
-Widget dButtonOutlined({required onPressed, String text = ""}){
-  return OutlinedButton(
-    onPressed: onPressed, 
-    child: Text(text),
-    style: OutlinedButton.styleFrom(foregroundColor: outlinedColor),
-    );
 }
