03a7b978f07159ed8c67eb5bc6c384a33a4b8b4d
xue <behicsakar@gmail.com>
2024-06-11 18:35:19 +0300

    refactor: Refactor dButtonFilled in dfault_buttons.dart for improved readability and maintainability

--- lib/dfault_framework/dfault_material/dfault_buttons.dart
+++ lib/dfault_framework/dfault_material/dfault_buttons.dart
@@ -3,19 +3,18 @@ import 'package:flutter/material.dart';
 //default ButtonFilled parameters
 const Color inactiveColor = Color.fromARGB(221, 114, 113, 113);
 
-
-Widget dButtonFilled({required onPressed, String text = "", bool active = true}){
-
+Widget dButtonFilled(
+    {required onPressed, String text = "", bool active = true}) {
   if (active == false) {
     return ElevatedButton(
-      onPressed: onPressed, 
-      child: Text(text), 
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(backgroundColor: inactiveColor),
-      );
+      child: Text(text),
+    );
   } else {
     return ElevatedButton(
-      onPressed: onPressed, 
-      child: Text(text), 
-      );
+      onPressed: onPressed,
+      child: Text(text),
+    );
   }
 }
