f2b2bdb679861b4c6281b9c8f5aead0c0ebea7b9
xue <behicsakar@gmail.com>
2024-06-12 14:39:51 +0300

    refactor: Remove unnecessary comments and import LoginScreen instead of Home in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:langx_flutter/pages/home/bottom_navigation_bar.dart';
 import 'package:langx_flutter/login.dart';
 // import 'package:langx_flutter/old/community.dart';
 // import 'package:langx_flutter/pages/home/profile.dart';
