2e97751c48f213e6aa189b114170faa7a8764dcf
David <davidhvse@gmail.com>
2024-06-12 18:06:28 +0200

    Add the themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:langx_flutter/pages/home/bottom_navigation_bar.dart';
 import 'package:langx_flutter/login.dart';
+import 'package:langx_flutter/theme.dart';
 // import 'package:langx_flutter/old/community.dart';
 // import 'package:langx_flutter/pages/home/profile.dart';
 // import 'package:langx_flutter/pages/home/profile.dart';
@@ -15,7 +16,8 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: whiteTheme(),
+      darkTheme: darkTheme(),
       home: const LoginScreen(),
       // home: const Home(),
     );
