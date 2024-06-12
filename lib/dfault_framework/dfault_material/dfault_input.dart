b79cc3432840f38625421d59812b20dcb5919cb6
David <davidhvse@gmail.com>
2024-06-12 18:06:17 +0200

    Remove the style from dfault button
    It's now in the material theme so it's no longer needed there

--- lib/dfault_framework/dfault_material/dfault_input.dart
+++ lib/dfault_framework/dfault_material/dfault_input.dart
@@ -1,27 +1,9 @@
 import 'package:flutter/material.dart';
 
 dInput({required label}){
-  return Container(
-    decoration: BoxDecoration(
-      boxShadow:[ BoxShadow(
-        color: Color.fromARGB(255, 193, 193, 193).withOpacity(0.5),
-        spreadRadius: 2,
-        blurRadius: 10,
-        offset: Offset(0, 0), // cambios en la posición de la sombra
-      ),
-    ],
-    ),
-    child: TextField(
+  return TextField(
     decoration: InputDecoration(
       label: Text(label),
-      fillColor: Color.fromARGB(255, 234, 234, 234),
-      filled: true,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(15),
-        borderSide: const BorderSide(color: Colors.white
-          ),
-        )
       )
-    ),
-  );
+    );
 }
