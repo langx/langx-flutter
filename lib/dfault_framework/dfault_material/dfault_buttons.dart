30a871623a841503a7a4401457b04a95fb30bca4
David <davidhvse@gmail.com>
2024-06-12 18:08:36 +0200

    Adjust gradients and add const
    More "plain" UI buttons with some small adjustments and added const for an improved performance

--- lib/dfault_framework/dfault_material/dfault_buttons.dart
+++ lib/dfault_framework/dfault_material/dfault_buttons.dart
@@ -3,15 +3,14 @@ import 'package:flutter/material.dart';
 //default ButtonFilled parameters
 const Color inactiveColor = Color.fromARGB(221, 114, 113, 113);
 
-Widget dButtonFilled(
-    {required onPressed, String text = "", bool active = true}) {
+Widget dButtonFilled({required onPressed, String text = "", bool active = true}) {
     return Container(
             height: 60,
             decoration: BoxDecoration(
-              gradient: LinearGradient(
+              gradient: const LinearGradient(
                 colors: [
+                  Color.fromARGB(255, 222, 171, 2),
                   Color(0xFFFFC409),
-                  Color.fromARGB(255, 186, 143, 1),
                 ],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
@@ -21,10 +20,10 @@ Widget dButtonFilled(
               ),
               boxShadow: [
                 BoxShadow(
-                  color: Color(0xFFFFC409).withOpacity(0.2),
+                  color: const Color(0xFFFFC409).withOpacity(0.2),
                   spreadRadius: 4,
-                  blurRadius: 10,
-                  offset: Offset(0, 3),
+                  blurRadius: 11,
+                  offset: const Offset(0, 0),
                 )
               ]
             ),
@@ -34,7 +33,7 @@ Widget dButtonFilled(
                 child: Text(
                   text,
                   textAlign: TextAlign.left,
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontFamily: "Netflix",
                     fontWeight: FontWeight.w600,
                     fontSize: 18,
