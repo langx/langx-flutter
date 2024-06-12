85ff8ba34e8d3272150ae9ad117c29812dd6e7e4
xue <behicsakar@gmail.com>
2024-06-12 22:09:37 +0300

    fix: format, prettier

--- lib/dfault_framework/dfault_material/dfault_buttons.dart
+++ lib/dfault_framework/dfault_material/dfault_buttons.dart
@@ -4,44 +4,43 @@ import 'package:flutter/material.dart';
 const Color inactiveColor = Color.fromARGB(221, 114, 113, 113);
 
 Widget dButton({required onTap, String text = "", bool active = true}) {
-    return Container(
-            height: 60,
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 222, 171, 2),
-                  Color(0xFFFFC409),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(15),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0xFFFFC409).withOpacity(0.2),
-                  spreadRadius: 4,
-                  blurRadius: 11,
-                  offset: const Offset(0, 0),
-                )
-              ]
-            ),
-      child: Center(
-        child: GestureDetector(
-          onTap: onTap,
-          child: Text(
-            text,
-            textAlign: TextAlign.left,
-            style: const TextStyle(
-              fontFamily: "Netflix",
-              fontWeight: FontWeight.w600,
-              fontSize: 18,
-              letterSpacing: 0.0,
-              color: Colors.white,
-            ),
+  return Container(
+    height: 60,
+    decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            Color.fromARGB(255, 222, 171, 2),
+            Color(0xFFFFC409),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xFFFFC409).withOpacity(0.2),
+            spreadRadius: 4,
+            blurRadius: 11,
+            offset: const Offset(0, 0),
+          )
+        ]),
+    child: Center(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Text(
+          text,
+          textAlign: TextAlign.left,
+          style: const TextStyle(
+            fontFamily: "Netflix",
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
+            letterSpacing: 0.0,
+            color: Colors.white,
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
