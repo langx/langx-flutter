420f150885cd47fe3b6e895d24bcf086023ae435
xue <behicsakar@gmail.com>
2024-06-12 15:07:37 +0300

    feat: appwrite sdk implemented. Update main.dart to import LoginScreen instead of Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:appwrite/appwrite.dart';
+
 import 'package:langx_flutter/login.dart';
 // import 'package:langx_flutter/old/community.dart';
 // import 'package:langx_flutter/pages/home/profile.dart';
@@ -6,16 +8,30 @@ import 'package:langx_flutter/login.dart';
 // import 'package:langx_flutter/pages/home/home.dart';
 
 void main() {
-  runApp(const Main());
+  WidgetsFlutterBinding.ensureInitialized();
+  Client client =
+      Client().setEndpoint("https://db.langx.io/v1").setProject("<PROJECT_ID>");
+  Account account = Account(client);
+
+  runApp(Main(
+    home: const LoginScreen(),
+    account: account,
+  ));
 }
 
 class Main extends StatelessWidget {
-  const Main({super.key});
+  final Widget home;
+  final Account account;
+
+  // ignore: use_super_parameters
+  const Main({Key? key, required this.home, required this.account})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: const LoginScreen(),
+      home: home,
       // home: const Home(),
     );
   }
