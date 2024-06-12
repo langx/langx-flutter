a4f2d566bd497afcf80bca1f5a2bccde1c47edb7
David <davidhvse@gmail.com>
2024-06-12 13:04:15 +0200

    Added dfault buttons and imput

--- lib/dfault_framework/dfault_material/dfault_buttons.dart
+++ lib/dfault_framework/dfault_material/dfault_buttons.dart
@@ -5,16 +5,44 @@ const Color inactiveColor = Color.fromARGB(221, 114, 113, 113);
 
 Widget dButtonFilled(
     {required onPressed, String text = "", bool active = true}) {
-  if (active == false) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(backgroundColor: inactiveColor),
-      child: Text(text),
-    );
-  } else {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(text),
-    );
-  }
+    return Container(
+            height: 60,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFFFC409),
+                  Color.fromARGB(255, 186, 143, 1),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFFFFC409).withOpacity(0.2),
+                  spreadRadius: 4,
+                  blurRadius: 10,
+                  offset: Offset(0, 3),
+                )
+              ]
+            ),
+            child: Center(
+              child: GestureDetector(
+                onTap: () {},
+                child: Text(
+                  text,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: "Netflix",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    letterSpacing: 0.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          );
 }
