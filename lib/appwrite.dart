099ce106782f7175f35e10feca848e404bdf669f
David <davidhvse@gmail.com>
2024-06-13 15:37:09 +0200

    Created appwrite.dart with the main config

---
+++ lib/appwrite.dart
@@ -0,0 +1,6 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Client client = Client()
+    .setEndpoint(dotenv.env['APP_ENDPOINT']!)
+    .setProject(dotenv.env['APP_PROJECT']!);   
