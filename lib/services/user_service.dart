8acf7db286b86bdf7e33344da29cecad2e78c4c2
xue <behicsakar@gmail.com>
2024-06-28 08:09:38 +0300

    refactor: Update listUsers function to include query for ordering by updatedAt

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,3 +1,4 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -6,7 +7,8 @@ import 'package:langx_flutter/services/api_service.dart';
 
 Future<List<dynamic>> listUsers() async {
   try {
-    final response = await listDocuments(dotenv.env['USERS_COLLECTION']!);
+    final response = await listDocuments(dotenv.env['USERS_COLLECTION']!,
+        queries: [Query.orderDesc('\$updatedAt')]);
     return response.documents;
   } catch (e) {
     debugPrint('Error listing users: $e');
