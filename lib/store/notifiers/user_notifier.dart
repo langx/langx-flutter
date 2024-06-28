79725aabe1abaec2b8195ed65abc538e33eda6ac
xue <behicsakar@gmail.com>
2024-06-28 07:48:54 +0300

    feat: Add UserNotifier to fetch and store users from UserService

---
+++ lib/store/notifiers/user_notifier.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:langx_flutter/services/user_service.dart';
+
+class UserNotifier extends StateNotifier<List<dynamic>> {
+  UserNotifier() : super([]);
+
+  Future<void> fetchUsers() async {
+    try {
+      final users = await listUsers();
+      state = users;
+    } catch (e) {
+      state = [];
+      debugPrint('Failed to fetch users: $e');
+    }
+  }
+}
