a5c1a59879f7c1923b2b7e8959fa88146fb7c9ef
xue <behicsakar@gmail.com>
2024-06-29 05:44:36 +0300

    refactor: Update login page layout and form padding

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -47,110 +47,118 @@ class LoginScreenState extends ConsumerState<LoginScreen> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Form(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.all(25),
-                      padding: const EdgeInsets.all(5),
-                      child: Image.asset(
-                        platformBrightness == Brightness.dark
-                            ? "assets/images/langx-white.png"
-                            : "assets/images/langx-dark.png",
-                      ),
-                    ),
-                    const SizedBox(height: 25),
-                    dInput(
-                      label: "Email",
-                      controller: emailController,
-                    ),
-                    const SizedBox(height: 16),
-                    dInput(
-                      label: "Password",
-                      controller: passwordController,
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 35),
-                    dButton(
-                      text: "Login",
-                      onPress: () async {
-                        await authNotifier.login(
-                          email: emailController.text,
-                          password: passwordController.text,
-                          context: context,
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 16),
-                    TextButton(
-                      onPressed: () {
-                        // Navigate to registration page
-                      },
-                      child: const Text('Not a member yet? SIGN UP'),
-                    ),
-                    const SizedBox(height: 8),
-                    TextButton(
-                      onPressed: () {
-                        // Navigate to forgot password page
-                      },
-                      child: const Text('Forgot password?'),
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+          child: Padding(
+            padding: const EdgeInsets.all(
+                20.0), // Optional padding for better spacing
+            child: Card(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Form(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        IconButton(
-                          icon: SvgPicture.asset(
-                            'assets/images/google_icon.svg',
-                            width: 40,
-                            height: 40,
+                        Container(
+                          margin: const EdgeInsets.all(25),
+                          padding: const EdgeInsets.all(5),
+                          child: Image.asset(
+                            platformBrightness == Brightness.dark
+                                ? "assets/images/langx-white.png"
+                                : "assets/images/langx-dark.png",
                           ),
-                          onPressed: () async {
-                            await authNotifier.oAuthLogin(
-                              provider: OAuthProvider.google,
+                        ),
+                        const SizedBox(height: 25),
+                        dInput(
+                          label: "Email",
+                          controller: emailController,
+                        ),
+                        const SizedBox(height: 16),
+                        dInput(
+                          label: "Password",
+                          controller: passwordController,
+                          obscureText: true,
+                        ),
+                        const SizedBox(height: 35),
+                        dButton(
+                          text: "Login",
+                          onPress: () async {
+                            await authNotifier.login(
+                              email: emailController.text,
+                              password: passwordController.text,
                               context: context,
                             );
                           },
                         ),
-                        const SizedBox(width: 30),
-                        IconButton(
-                          icon: SvgPicture.asset(
-                            'assets/images/facebook_icon.svg',
-                            width: 40,
-                            height: 40,
-                          ),
-                          onPressed: () async {
-                            await authNotifier.oAuthLogin(
-                              provider: OAuthProvider.facebook,
-                              context: context,
-                            );
+                        const SizedBox(height: 16),
+                        TextButton(
+                          onPressed: () {
+                            // Navigate to registration page
                           },
+                          child: const Text('Not a member yet? SIGN UP'),
                         ),
-                        const SizedBox(width: 30),
-                        IconButton(
-                          icon: SvgPicture.asset('assets/images/apple_icon.svg',
-                              width: 40, height: 40),
-                          onPressed: () async {
-                            await authNotifier.oAuthLogin(
-                              provider: OAuthProvider.apple,
-                              context: context,
-                            );
+                        const SizedBox(height: 8),
+                        TextButton(
+                          onPressed: () {
+                            // Navigate to forgot password page
                           },
+                          child: const Text('Forgot password?'),
+                        ),
+                        const SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: SvgPicture.asset(
+                                'assets/images/google_icon.svg',
+                                width: 40,
+                                height: 40,
+                              ),
+                              onPressed: () async {
+                                await authNotifier.oAuthLogin(
+                                  provider: OAuthProvider.google,
+                                  context: context,
+                                );
+                              },
+                            ),
+                            const SizedBox(width: 30),
+                            IconButton(
+                              icon: SvgPicture.asset(
+                                'assets/images/facebook_icon.svg',
+                                width: 40,
+                                height: 40,
+                              ),
+                              onPressed: () async {
+                                await authNotifier.oAuthLogin(
+                                  provider: OAuthProvider.facebook,
+                                  context: context,
+                                );
+                              },
+                            ),
+                            const SizedBox(width: 30),
+                            IconButton(
+                              icon: SvgPicture.asset(
+                                  'assets/images/apple_icon.svg',
+                                  width: 40,
+                                  height: 40),
+                              onPressed: () async {
+                                await authNotifier.oAuthLogin(
+                                  provider: OAuthProvider.apple,
+                                  context: context,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 30),
+                        TextButton(
+                          onPressed: () {
+                            // Show introduction
+                          },
+                          child: const Text('SHOW INTRO'),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 30),
-                    TextButton(
-                      onPressed: () {
-                        // Show introduction
-                      },
-                      child: const Text('SHOW INTRO'),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
